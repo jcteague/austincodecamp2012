6b7200f3310fe573a52bf66bdcec476484239cb6
John Teague <jcteague@gmail.com>
2012-06-07 14:36:27 -0500

    added gzip

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.configure(function(){
 app.configure('production',function(){
     mongoose.connect(process.env["MONGOHQ_URL"]);
     app.use(express.errorHandler());
-    //app.use(express.gzip());
+    app.use(express.gzip());
     app.use(express.static(path + '/public',{maxAge:60000000}));  // Before router to enable dynamic routing
 });
 
